2231780d89b95cdb44a671f9faa48cd04b3df933
Ahmed Yousef <ahmedmmy97@gmail.com>
2020-03-03 19:51:12 +0300

    Updates

---
+++ precache-manifest.917acd75332a3c5956f92e4ddcd5c611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9907531bff5b5c15fcfeac80e313664a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "28e13f7c2edb53d6a31a",
+    "url": "/monsters-rolodex/static/css/main.36093d24.chunk.css"
+  },
+  {
+    "revision": "f42723a82595d0b3b155",
+    "url": "/monsters-rolodex/static/js/2.da2b909b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.da2b909b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e13f7c2edb53d6a31a",
+    "url": "/monsters-rolodex/static/js/main.b159edd5.chunk.js"
+  },
+  {
+    "revision": "c8e297be211d3b740a44",
+    "url": "/monsters-rolodex/static/js/runtime-main.4a576d0d.js"
+  }
+]);
